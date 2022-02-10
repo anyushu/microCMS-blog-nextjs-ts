c5df180af12ee4a1bf5209d64cb0c4c1ba717854
anyushu <suzuki@lamp.jp>
2022-02-10 18:11:03 +0900

    next/image が使えるように

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,7 @@
+import * as NextImage from 'next/image'
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
@@ -7,3 +9,10 @@ export const parameters = {
     },
   },
 }
+
+const OriginalNextImage = NextImage.default
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+})
