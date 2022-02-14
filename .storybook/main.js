18eb18809d9f6fae2320be486c60c55806677cb1
anyushu <suzuki@lamp.jp>
2022-02-14 14:29:30 +0900

    #26 add: tsconfig-paths-webpack-plugin

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const path = require('path')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -12,6 +15,11 @@ module.exports = {
       },
     },
   ],
+  webpackFinal(config) {
+    config.resolve.modules = [...(config.resolve.modules || []), path.resolve(__dirname, '../src')]
+    config.resolve.plugins = [...(config.resolve.plugins || []), new TsconfigPathsPlugin()]
+    return config
+  },
   framework: '@storybook/react',
   core: {
     builder: 'webpack5',
