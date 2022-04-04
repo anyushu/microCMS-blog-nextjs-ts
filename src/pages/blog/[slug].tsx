8bb63654f8c888507df71ccc22e0f81d3f05ff59
anyushu <suzuki@lamp.jp>
2022-04-04 14:41:37 +0900

    change next-seo.config

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -17,7 +17,7 @@ import Container from '@/components/atoms/Container'
 import PostHeader from '@/components/molecules/post/PostHeader'
 import { options } from '@/libs/html-to-react-parser'
 import { getBlog, getBlogList } from '@/libs/microcms/get-blog'
-import { siteTitle } from '@/next-seo.config'
+import { siteTitle } from '@/libs/next-seo.config'
 import { blog } from '@/types/cms-types'
 import createOgp from '@/utils/server/ogp'
 
